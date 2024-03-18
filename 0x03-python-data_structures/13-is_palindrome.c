75262825bb42b5fab39f627eac72f61990942fa4
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-03-18 04:04:21 +0200

    lists,tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * palindrom - a function that recursive palind or not
+ * is_palindrom - a function that recursive palind or not
  * @head: pointer to the head of linked list
  * Return: 1 if is palindrome, otherwise return 0
 */
 
 int is_palindrom(listint_t **head)
 {
-    if (head == NULL || *head == NULL)
-        return (1);
-    return (aux_palind(head, *head));
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (aux_palind(head, *head));
 }
 
 /*
@@ -22,12 +22,12 @@ int is_palindrom(listint_t **head)
 
 aux_palind(listint_t **head, listint_t *end)
 {
-    if (end == NULL)
-        return (1);
-    if (aux_palind(head, end->next) && (*head)->n == end->n)
-    {
-        (*head) = (*head)->next;
-        return (1);
-    }
-    return (0);
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	{
+		(*head) = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -7,7 +7,7 @@
  * @h: pointer to head of list
  * Return: number of nodes
  */
- 
+
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
