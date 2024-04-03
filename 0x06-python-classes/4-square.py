501e9b3d971797df7d4309a57cc0cdc4587521e8
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-04-03 03:08:29 +0200

    classes and objects task

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """property for the length of a side of this square.
+        raises:
+            TypeError: If size is not an integer.
+            ValueEroor: If size is less than 0.
+            """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Area of this square.
+        Returns:
+            The area of this square.
+        """
+        return self.__size ** 2
