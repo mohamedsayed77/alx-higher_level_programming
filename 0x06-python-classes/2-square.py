2d752750241063e1fc93d0c0f2cc50ec2b677190
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-04-03 02:51:48 +0200

    classes and objects task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,15 +4,15 @@
 
 class Square:
     """Defines a square."""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Constructor.
 
         Args:
-            size: length of a side of the square.
+            size: Length of a side of the square.
 
         Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is less than 0
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
         """
         if not isinstance(size, int):
             raise TypeError("Size must be an integer.")
