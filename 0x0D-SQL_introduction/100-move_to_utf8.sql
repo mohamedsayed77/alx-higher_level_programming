3907f4ef659ae43a888704e5c378ab2881d0b642
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-05-23 04:19:07 +0300

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++
@@ -1,8 +0,0 @@
--- Change the character set and collation of the database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the character set and collation of the table 'first_table' to utf8mb4
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Modify the character set and collation of the column 'name' in 'first_table' to utf8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/102-top_city.sql
+++
@@ -1,7 +0,0 @@
--- Calculate the average temperature (Fahrenheit) for each city during July and August, and display the top 3 cities ordered by temperature (descending)
-SELECT city, AVG(temperature) AS avg_temp
-FROM table_name
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
