7ff3b8a46f101638c1d4971970e47dafe14e482d
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-05-23 04:11:03 +0300

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,8 @@
-USE hbtn_0c_0;
+-- Change the character set and collation of the database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- Convert the table to utf8mb4
+-- Convert the character set and collation of the table 'first_table' to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field 'name' to utf8mb4
+-- Modify the character set and collation of the column 'name' in 'first_table' to utf8mb4
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
