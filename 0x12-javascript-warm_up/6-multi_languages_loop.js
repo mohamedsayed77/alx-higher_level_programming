bf8c5b9d49cea639b9a3def2539b43d4929e8987
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-06-18 22:29:48 +0300

    solv

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints 3 lines, similar to 1-multi_languages.js, but using an array of strings and a loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
