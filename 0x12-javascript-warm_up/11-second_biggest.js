f88ea8d25d2e8743e09a74a9ad23f92619a4d130
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-06-18 22:41:41 +0300

    solv

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,26 +2,26 @@
 // Searches the second biggest integer in the list of arguments.
 
 if (process.argv.length <= 2) {
-  console.log(0);
+	console.log(0);
 } else if (process.argv.length === 3) {
-  console.log(0);
+	console.log(0);
 } else {
-  const numbers = process.argv.slice(2).map(Number).filter(n => !isNaN(n));
-  let max = -Infinity;
-  let secondMax = -Infinity;
+	const numbers = process.argv.slice(2).map(Number).filter(n => !isNaN(n));
+	let max = -Infinity;
+	let secondMax = -Infinity;
 
-  for (let num of numbers) {
-    if (num > max) {
-      secondMax = max;
-      max = num;
-    } else if (num > secondMax && num !== max) {
-      secondMax = num;
-    }
-  }
+	for (let num of numbers) {
+		if (num > max) {
+			secondMax = max;
+			max = num;
+		} else if (num > secondMax && num !== max) {
+			secondMax = num;
+		}
+	}
 
-  if (secondMax === -Infinity) {
-    console.log(0);
-  } else {
-    console.log(secondMax);
-  }
+	if (secondMax === -Infinity) {
+		console.log(0);
+	} else {
+		console.log(secondMax);
+	}
 }
