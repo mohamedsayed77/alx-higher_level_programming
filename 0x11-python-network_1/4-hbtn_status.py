9d4047748531d413409ab8df377ef4679209b747
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-07-06 02:59:11 +0300

    pyhton network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
 if __name__ == '__main__':
-
-    res = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
